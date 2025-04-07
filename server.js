9530acdbb44c526fb40319ef58044d7de351b2b8
Koop Reynders <k.g.reynders@hva.nl>
2025-04-08 00:29:02 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// require('dotenv').config()
+// const PORT = config.env.PORT || 3000
+// console.log("PORT " + PORT)
+
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
 import express from 'express'
 
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+import { Liquid } from 'liquidjs'
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
@@ -17,7 +21,7 @@ app.use(express.static('public'))
 
 // Stel Liquid in als 'view engine'
 const engine = new Liquid();
-app.engine('liquid', engine.express());
+app.engine('liquid', engine.express())
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
